0509ef815e86c2a2a93ed36ddeb7f8cafca70042
gngo2018 <gngo2018@gmail.com>
2019-01-06 20:51:56 -0500

    first commit

---
+++ src/SIS.Database.DataContract/Note/NoteCreateRAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedStarter.Database.DataContract.Note
+{
+    public class NoteCreateRAO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset DateCreate { get; set; }
+    }
+}
